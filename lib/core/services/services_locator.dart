3f424dcd0d61aaaeea3c9947200e0d55de1f8aa4
Chihab-Dev <millor.1m.elhak@gmail.com>
2024-02-22 19:24:18 +0100

    [ADD] network info to service locator

--- lib/core/services/services_locator.dart
+++ lib/core/services/services_locator.dart
@@ -1,7 +1,9 @@
 import 'package:get_it/get_it.dart';
 import 'package:ghack_app/data/data_source/remote_data_source.dart';
+import 'package:ghack_app/data/network/network_info.dart';
 import 'package:ghack_app/data/repository/repository.dart';
 import 'package:ghack_app/domain/repository/repository.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 final getIt = GetIt.instance;
 
@@ -10,7 +12,10 @@ class ServiceLocator {
     // Remote Data Source
     getIt.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImpl());
 
+    // Network info
+    getIt.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(InternetConnectionChecker()));
+
     // Repository
-    getIt.registerLazySingleton<Repository>(() => RepositoryImpl());
+    getIt.registerLazySingleton<Repository>(() => RepositoryImpl(getIt(), getIt()));
   }
 }
