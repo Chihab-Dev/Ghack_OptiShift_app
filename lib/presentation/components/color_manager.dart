bac75f2c43f82815d99695c03884533da5d75205
Chihab-Dev <millor.1m.elhak@gmail.com>
2024-02-22 21:39:18 +0100

    [ADD] colors components

--- lib/presentation/components/color_manager.dart
+++ lib/presentation/components/color_manager.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 class ColorManager {
   // static Color color = const Color(0xffCOLORCODE);
   static Color yellow = const Color(0xffFFC718);
+  static Color white = const Color(0xffFFFFFF);
+  static Color dark = const Color(0xff000000);
+  static Color green = const Color(0xff69BC78);
+  static Color grey = const Color(0xffECEDEF);
 }
