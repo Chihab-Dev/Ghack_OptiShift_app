9d681b8f75f13e37040a7421a96df696606feefb
Chihab-Dev <millor.1m.elhak@gmail.com>
2024-02-22 19:41:34 +0100

    [ADD] ScreenUtilInit for responsive screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ghack_app/core/services/services_locator.dart';
+import 'package:ghack_app/presentation/components/theme_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await ServiceLocator().init();
+
+  // Only vertical screens :
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MainApp());
 }
 
@@ -9,12 +23,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return ScreenUtilInit(
+      // emulator width and height (13 pro max )
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      useInheritedMediaQuery: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: getApplicationTheme(),
+          home: const Scaffold(
+            body: Center(
+              child: Text('Hello World!'),
+            ),
+          ),
+        );
+      },
     );
   }
 }
